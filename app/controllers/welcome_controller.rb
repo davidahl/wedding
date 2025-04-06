838fb19c01139d312ac409d28060b8732f20dea6
David Ahl <david.ahl.axelsson@gmail.com>
2025-04-06 19:00:49 +0200

    change info, update map tag, update config

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,7 +2,7 @@
 
 class WelcomeController < ApplicationController
   def index
-    @google_maps_url = 'https://maps.app.goo.gl/L9oEjWzbouT5e3Eg9'
-    @google_maps_url_party = 'https://maps.app.goo.gl/3ua8CWntKijK9NnDA'
+    @google_maps_url_vigsel = 'https://maps.app.goo.gl/L9oEjWzbouT5e3Eg9'
+    @google_maps_url_party = 'https://maps.app.goo.gl/ibmJ1KXi6pNJzVWPA'
   end
 end
